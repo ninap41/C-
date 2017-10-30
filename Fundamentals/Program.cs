62b66be49e3ce904d4799da4ab753f7404d585eb
ninap41 <ninapalumbo@gmail.com>
2017-10-30 12:18:10 -0500

    "Added Fundamentals"

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -12,12 +12,28 @@ namespace Fundamentals
             for(int val= 1; val <= 255; val++){
                 Console.WriteLine(val);
             }
-            //print values divisible by 3 and 5 but not both
+            //Print values divisible by 3 and 5 but not both
+            for(int val= 1; val <= 100; val++){
+                if(val % 3 == 0 && val % 5 != 0)
+                Console.WriteLine(val);
+                if(val % 5 == 0 && val % 3 != 0)
+                Console.WriteLine(val);
+            }
+            //Print without modulus
             for(int val= 1; val <= 100; val++){
                 if(val % 3 == 0 && val % 5 != 0)
                 Console.WriteLine(val);
             }
-            
+
+
+            //modify to print "Fizz" for multiples of 3. multiples of 5 "Buzz"
+             for(int val= 1; val <= 100; val++){
+                if(val % 3 == 0 && val % 5 != 0)
+                Console.WriteLine("Fizz");
+                if(val % 5 == 0 && val % 3 != 0)
+                Console.WriteLine("Buzz");
+            }
+
 
 
         }
