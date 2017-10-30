77d71bd1ba607c6027dc05e940fb9373df8a548f
ninap41 <ninapalumbo@gmail.com>
2017-10-30 12:14:43 -0500

    added first assignment

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // Print all the valeus between 1- 255
+            for(int val= 1; val <= 255; val++){
+                Console.WriteLine(val);
+            }
+            //print values divisible by 3 and 5 but not both
+            for(int val= 1; val <= 100; val++){
+                if(val % 3 == 0 && val % 5 != 0)
+                Console.WriteLine(val);
+            }
+            
+
+
+        }
+    }
+}
--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -7,9 +7,43 @@ namespace first_csharp
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
-            Console.WriteLine("100");
-            Console.WriteLine(100);
+            // Console.WriteLine("100");
+            // Console.WriteLine(100);
+            // Console.WriteLine("The {0} cow, jumped over the {1}, {2} times!" + "{0}", "brown", "Moon", 7);
 
+            int rings = 5;
+            if (rings >= 5){
+                Console.WriteLine("You are welcome to join the party");
+                
+
+            }
+            else if (rings > 2){
+            Console.WriteLine("Decent...but {0} rings aren't enough", rings);
+            }
+            else {
+                Console.WriteLine("Go win some more rings");
+             }
+
+            // bool crazy = true;
+            // if (!crazy){
+            // Console.WriteLine("Let's party!");
+            // }
+
+            Random num = new Random();
+            for(int val=0; val<10; val++)
+            {
+                Console.WriteLine(num.Next(5,10));
+            }
+            // Vs
+            for(int val = 0; val < 2; val++){
+                Random rand  = new Random();
+                //This will print the same generated value each time!!!
+                Console.WriteLine(rand.Next(2,8));
+            }
+
+
+
+           
         }
     }
 }
