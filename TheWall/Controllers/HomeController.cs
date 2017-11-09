74d232d333192dc9f8e1875e779b38ce64cdf5d4
ninap41 <ninapalumbo@gmail.com>
2017-11-09 11:49:57 -0600

    My LOGIN iS PERFECT and BEAUTIFUL:3

--- TheWall/Controllers/HomeController.cs
+++ TheWall/Controllers/HomeController.cs
@@ -3,17 +3,86 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using newloginreg.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Razor;
 
-namespace TheWall.Controllers
+namespace newloginreg.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DbConnector _dbConnector;
+ 
+        public HomeController(DbConnector connect)
+        {
+            _dbConnector = connect;
+        }
+
         // GET: /Home/
         [HttpGet]
         [Route("")]
-        public IActionResult Index()
-        {
+        public IActionResult Index(){
+        string query ="SELECT * FROM new_users";
+            List<Dictionary<string,object>> result = _dbConnector.Query(query);
+            ViewBag.allUsers = result;
+       
             return View();
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(User user)
+        {
+            if(ModelState.IsValid){
+                try 
+                {
+                    List<Dictionary<string, object>> RegisteredUser = _dbConnector.Query($"SELECT * FROM new_users WHERE email='{user.Email}'");
+                    var CurrentUser = RegisteredUser[0];
+                                // PasswordHasher<User> hasher = new PasswordHasher<User>();
+            // PasswordHasher<User> hasher = new PasswordHasher<User>();
+            // string hashed = hasher.HashPassword(user, user.Password);
+                    if((string)CurrentUser["email"] == user.Email){
+                        ViewBag.Error = "This email already belongs to a user!";
+                        return View("Index");
+                    }
+                }
+                catch 
+                {
+                    _dbConnector.Execute($"INSERT INTO new_users (first_name, last_name, email, password) VALUES('{user.FirstName}', '{user.LastName}', '{user.Email}', '{user.Password}')");
+
+                    List<Dictionary<string, object>> CurrentUser = _dbConnector.Query($"SELECT * FROM new_users WHERE email='{user.Email}'");
+                    
+                    HttpContext.Session.SetString("UserName", user.FirstName);
+                    ViewBag.UserName = user.FirstName;
+
+                    return View("Success");
+                }
+                
+            }
+
+            return View("Index", user);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(string email, string password)
+        {
+            if(ModelState.IsValid){
+
+                List<Dictionary<string, object>> RegisteredUser = _dbConnector.Query($"SELECT * FROM new_users WHERE email='{email}'");
+                var user = RegisteredUser[0];
+                
+                if((string)user["email"] == email && (string)user["password"] == password){
+                    ViewBag.UserName = (string)user["first_name"];
+                    return View("Success");
+                }
+                else {
+                    ViewBag.Error = "That user is not in our database!";
+                    return View("Index");
+                } 
+            }
+
+            return View("Index");
         }
     }
 }
--- TheWall/Startup.cs
+++ TheWall/Startup.cs
@@ -2,17 +2,29 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
 
-namespace TheWall
+
+
+namespace newloginreg
 {
     public class Startup
     {
+    
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
             services.AddSession();
+            services.Configure<MySqlOptions>(Configuration.GetSection("DBInfo"));
+            services.AddScoped<DbConnector>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,6 +35,16 @@ namespace TheWall
             app.UseStaticFiles();
             app.UseSession();
             app.UseMvc();
+        }
+            public IConfiguration Configuration { get; private set; }
+ 
+        public Startup(IHostingEnvironment env)
+        {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+        Configuration = builder.Build();
         }
     }
 }
